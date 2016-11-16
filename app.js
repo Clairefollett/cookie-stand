88b0f93e35b71d89e667e2d213d082cb1792c122
Clarefollett <chlaw101@gmail.com>
2016-11-16 14:47:04 -0800

    finished my constructors

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 'use strict';
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-//Example
-//function renderHeaderRow() {
-  //var storeTable = document.getElementById('store_table');
-  //var tableRow = document.createElement('tr');
-  //var blankTableHeader = document.createElement('th');
-  //var totalTableHeader = document.createElement('th');
-  //var hourlyTableHeader;
 
-  //tableRow.appendChild(blankTableHeader);
+function renderHeaderRow() {
+  var storeTable = document.getElementById('store_head');
+  var tableRow = document.createElement('tr');
+  var blankTableHeader = document.createElement('th');
+  var totalTableHeader = document.createElement('th');
+  var hourlyTableHeader;
 
-  //for (var i = 0; i < hours.length; i++) {
-    //hourlyTableHeader = document.createElement('th');
-    //hourlyTableHeader.textContent = hours[i];
-    //tableRow.appendChild(hourlyTableHeader);
-  //}
+  tableRow.appendChild(blankTableHeader);
 
-  //totalTableHeader.textContent = 'Total';
-  //tableRow.appendChild(totalTableHeader);
+  for (var i = 0; i < hours.length; i++) {
+    hourlyTableHeader = document.createElement('th');
+    hourlyTableHeader.textContent = hours[i];
+    tableRow.appendChild(hourlyTableHeader);
+  }
 
-  //storeTable.appendChild(tableRow);
-//}
+  totalTableHeader.textContent = 'Total';
+  tableRow.appendChild(totalTableHeader);
+
+  storeTable.appendChild(tableRow);
+}
 
 function CookieStore (storeName, minCust, maxCust, avgCookie) {
   this.name = storeName;
@@ -33,38 +33,13 @@ function CookieStore (storeName, minCust, maxCust, avgCookie) {
 CookieStore.prototype.logStoreName = function() {
   console.log(this.name);
 };
-CookieStore.prototype.toHtml = function() {
-  var storeTable = document.getElementById('store_table');
-  var tableRow = document.createElement('tr');
-  var nameTableHeader = document.createElement('th');
-  var totalTableData = document.createElement('td');
-  var hourlyTableData;
-
-  nameTableHeader.textContent = this.name;
-  tableRow.appendChild(nameTableHeader);
-
-  for (var i = 0; i < this.hours.length; i++) {
-    hourlyTableData = document.createElement('td');
-    hourlyTableData.textContent = 5;
-    tableRow.appendChild(hourlyTableData);
-  }
-
-  totalTableData.textContent = 15;
-  tableRow.appendChild(totalTableData);
-
-  console.log(tableRow, storeTable);
-
-  storeTable.appendChild(tableRow);
-};
-
 CookieStore.prototype.getRandomInt = function () {
   var min = Math.ceil(this.minCust);
   var max = Math.floor(this.maxCust);
   return Math.floor(Math.random() * (max - min)) + min;
 };
 CookieStore.prototype.totalCookiesHourly = function() {
-  var cookiesPerHour = (this.avgCookie * this.getRandomInt());
-  return cookiesPerHour;
+  this.cookieArray.push(Math.floor(this.avgCookie * this.getRandomInt()));
 };
 CookieStore.prototype.listCookieAverage = function() {
   var store_body = document.getElementById('store_body');
@@ -77,14 +52,27 @@ CookieStore.prototype.listCookieAverage = function() {
   tableRow.appendChild(nameTableData);
   var total = 0;
 
-  for(var i = 0; i < hours.length; i++); {
-
+  for (var i = 0; i < hours.length; i++) {
+    this.totalCookiesHourly();
+    var cookieCount = this.cookieArray[i];
     var avgCookiePerDay = document.createElement('td');
-    total += this.cookieArray[i];
-    avgCookiePerDay.textContent = this.cookieArray[i];
+    total += cookieCount;
+    avgCookiePerDay.textContent = cookieCount;
+    console.log(this.cookieArray);
     tableRow.appendChild(avgCookiePerDay);
   }
   totalColumn.textContent = total;
   tableRow.appendChild(totalColumn);
 };
-var Pike = new CookieStore('1st and Pike', 23, 65, 6.3);
+var pike = new CookieStore('1st and Pike', 23, 65, 6.3);
+var seaTac = new CookieStore('SeaTac Airport',3, 24, 1.2);
+var seattle = new CookieStore('Seattle Center', 11, 38, 3.7);
+var capitolHill = new CookieStore('Capitol Hill',20, 38, 2.3);
+var alki = new CookieStore ('Alki', 2, 16, 4.6);
+
+renderHeaderRow();
+pike.listCookieAverage();
+seaTac.listCookieAverage();
+seattle.listCookieAverage();
+capitolHill.listCookieAverage();
+alki.listCookieAverage();
